4ea93609bbd9f0e8dcd6b7591556cb17da0543fb
ane-circumspect <cathy_campbell@live.com>
2020-08-04 14:17:06 -0400

    A silly game for the kids

---
+++ Python3/sillygame.py
@@ -0,0 +1,12 @@
+name = input("Please give me your name:  ")
+print('Hello,', name,"! Let's play a game")
+print()
+number =['4','5','9']
+
+item = input('Guess a number between 1 and 10: ')
+if item in number:
+    print ('Congrats You Win')
+else:
+    print ('You lose!')
+
+ 
